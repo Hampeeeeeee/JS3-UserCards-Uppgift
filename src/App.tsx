06ae07753c4d022e2675aedc502be3488f9c5f9f
Hampeeeeeee <hampus_svensson99@hotmail.com>
2025-09-18 13:33:40 +0200

    Second commit.
    Displayed users.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import { useState } from 'react'
 import './App.css'
+import UserCardTanstack from './components/UserCardTanstack'
 
 function App() {
 
   return (
     <>
-      
+      <UserCardTanstack />
     </>
   )
 }
--- src/components/UserCardTanstack.tsx
+++ src/components/UserCardTanstack.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+// import React, { useContext } from "react"
 import { useQuery } from "@tanstack/react-query"
 import type { Users } from "../types/user"
 import LoadSpinner from "./LoadSpinner"
@@ -10,8 +10,7 @@ const UserCardTanstack = () => {
         isError, 
         error,
         isSuccess
-
-    }   = useQuery<Users>({
+    }   = useQuery<Users[]>({
         queryKey: ["users"],
         queryFn: () => fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
     })
@@ -19,17 +18,21 @@ const UserCardTanstack = () => {
     return (
         <>
             <h2>Usercards</h2>
+            <div>
             {isPending && <LoadSpinner />}
             {isError && <p>Something went wrong: {error.message}</p>}
             {isSuccess && users && (
                 <div>
-                    {users.map((user) =>
-                        <header>
-
-                        </header>
-                    )}
-                </div>
+                    {users.map(user => (
+                        <div key={user.id} className="user-card">
+                            <h3>{user.name}</h3>
+                        </div>
+                    ))}
+                    </div>
             )}
+            </div>
         </>
     )
 }
+
+export default UserCardTanstack
